f76bc18eda60de31b01fc5fd0d4bbee2b3fd829f
dariusalbu <dariusalbu3@gmail.com>
2026-05-07 15:22:18 +0300

    added architecture

---
+++ src/main/java/org/example/connection/ConnectionFactory.java
@@ -0,0 +1,4 @@
+package org.example.connection;
+
+public class ConnectionFactory {
+}
---
+++ src/main/java/org/example/start/Start.java
@@ -0,0 +1,7 @@
+package org.example.start;
+
+public class Start {
+    public static void main(String[] args) {
+
+    }
+}
